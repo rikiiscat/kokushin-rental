c9cd81bd1eda79189594fb515375a10bad49b3cb
sarah <xuelin2002@163.com>
2025-11-06 03:04:58 -0500

    fix enable SSL

--- backend/server.js
+++ backend/server.js
@@ -46,6 +46,9 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: true
+  }
 });
 
 db.connect((err) => {
